4b685ed31c61a22b438a0495042f7989241db769
ronke <ronkeashaju@gmail.com>
2023-09-26 04:11:07 -0700

    string

--- tests/6-main.js
+++ tests/6-main.js
@@ -1,3 +1,8 @@
+// Import the getSanFranciscoDescription function from description.js
 import getSanFranciscoDescription from '../6-string-interpolation.js';
 
+// Call the function to get the description
+const description = getSanFranciscoDescription();
+
+// Display the description
 console.log(getSanFranciscoDescription());
