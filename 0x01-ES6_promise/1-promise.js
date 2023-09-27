a98fff6aa085b3833085eac67759fd272826300a
ronke <ronkeashaju@gmail.com>
2023-09-27 08:53:55 -0700

    promise1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-	// resolve the promise with a successful response
+	// resolve the promise
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
