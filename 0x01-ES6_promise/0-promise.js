8cb739cb92085f67939f0b6ef7a32b304a26921c
ronke <ronkeashaju@gmail.com>
2023-09-27 08:16:51 -0700

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,20 +1,4 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Perform your asynchronous operation here
-    // For example, making an API request using fetch
-    fetch('https://api.example.com/data')
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('API request failed');
-        }
-      })
-      .then(data => {
-        resolve(data); // Resolve the Promise with the fetched data
-      })
-      .catch(error => {
-        reject(error); // Reject the Promise with an error if something goes wrong
-      });
+export default function getResponseFromAPI() {
+  return new Promise(() => {
   });
 }
