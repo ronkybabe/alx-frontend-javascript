00dbc0e11b4588b88de296920e5d06ce5ce4f110
ronke <ronkeashaju@gmail.com>
2023-09-27 14:34:47 -0700

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([createUser(), uploadPhoto()]).then((values) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
     console.log(
       `${values[1].body} ${values[0].firstName} ${values[0].lastName}`,
     );
