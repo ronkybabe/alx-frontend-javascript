c10dbdcd791901686706dde4affde473effccbe7
ronke <ronkeashaju@gmail.com>
2023-10-03 05:31:53 -0700

    students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+const getListStudents = () => [
+  // Each object represents a student and has properties like id, firstName, and location.
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+
+// Export the getListStudents function as the default export of this module.
+export default getListStudents;
