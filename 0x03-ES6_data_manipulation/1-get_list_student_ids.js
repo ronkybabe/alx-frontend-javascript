9163b661b5b9284a059f981820c3cb4477596db2
ronke <ronkeashaju@gmail.com>
2023-10-03 05:50:23 -0700

    list

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,10 @@
 const getListStudentIds = (array) => {
   // Check if the 'array' parameter is indeed an array.
   if (Array.isArray(array)) {
-     // Use the 'map' method to extract the 'id' property from each 'student' object in the array.
+     // If it is an array, use the 'map' method to extract the 'id' property from each 'student' object in the array.
     return array.map((student) => student.id);
   }
-  // Return the empty array.
+  // If the 'array' parameter is not an array, return an empty array.
   return [];
 };
 
