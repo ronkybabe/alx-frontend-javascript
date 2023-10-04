303a4cd4fba9151955a36a4bcde404245aaf39c2
ronke <ronkeashaju@gmail.com>
2023-10-04 02:03:23 -0700

    groceries

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,8 +1,11 @@
-const groceriesList = () => new Map ([
-  ['Apples', 10],
-  ['Tomatoes', 10],
-  ['Pasta', 1],
-  ['Rice', 1],
-  ['Banana', 5],
+// Define a function named groceriesList that returns a new Map containing grocery items and their quantities
+const groceriesList = () => new Map([
+  ['Apples', 10],      // Key: 'Apples', Value: 10
+  ['Tomatoes', 10],    // Key: 'Tomatoes', Value: 10
+  ['Pasta', 1],        // Key: 'Pasta', Value: 1
+  ['Rice', 1],         // Key: 'Rice', Value: 1
+  ['Banana', 5],       // Key: 'Banana', Value: 5
 ]);
+
+// Export the groceriesList function as the default export
 export default groceriesList;
