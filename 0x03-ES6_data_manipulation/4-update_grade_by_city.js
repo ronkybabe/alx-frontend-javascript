85205807505b320f571de21215187a7105c5f8a1
ronke <ronkeashaju@gmail.com>
2023-10-03 17:09:22 -0700

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,11 @@
-// This function updates the grades of students in a given city with new grades provided in the 'newGrades' array.
 const updateStudentGradeByCity = (students, city, newGrades) => {
-  // Check if 'students' and 'newGrades' are arrays; if not, return an empty array.
   if (!Array.isArray(students) || !Array.isArray(newGrades)) {
     return [];
   }
-  const studentsByCity = students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const grade = newGrades.find((grade) => grade.studentId === student.id);
-      return { ...student, grade: grade ? grade.grade : 'N/A' };
-    });
+  const studentsByCity = students.filter((student) => student.location === city).map((student) => {
+    const grade = newGrades.find((grade) => grade.studentId === student.id);
+    return { ...student, grade: grade ? grade.grade : 'N/A' };
+  });
 
   return studentsByCity;
 };
